bdbed235501ccf2d32ff0a152f927166d0101dcd
Franco Hervas <hervasfran02@gmail.com>
2025-07-25 10:42:40 -0300

    IDestroyables y DestroyablesManager

---
+++ Assets/Scripts/Environment/Destroyables/DestroyableWalls.cs
@@ -0,0 +1,23 @@
+
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DestroyableWalls : MonoBehaviour, IDestroyable
+{
+    //Este script es para cargar la interfaz a las paredes destruibles en la lista
+    //Hay que hacer uno de estos por cada lista creada de elementos destruibles
+    public void DestroyByInterface()
+    {
+        Destroy(gameObject);
+    }
+
+    void OnDestroy()
+    {
+        //Animaciones
+        //Sonidos
+        //Particulas
+        Debug.Log("Animacion");
+        Debug.Log("Sonido");
+        Debug.Log("Particulas");
+    }
+}
---
+++ Assets/Scripts/Managers/DestroyablesManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DestroyablesManager : MonoBehaviour
+{
+    //Aca van las listas de cosas destruibles
+    [SerializeField] List<GameObject> destroyableWallsList;
+
+    void Start()
+    {
+        DestroyWalls();
+    }
+    public void DestroyWalls()
+    {
+        foreach (var walls in destroyableWallsList)
+        {
+            walls.AddComponent<DestroyableWalls>();
+            print(walls.gameObject.name);
+        }
+    }
+
+    //Aca van los metodos de destruccion de cosas
+}
